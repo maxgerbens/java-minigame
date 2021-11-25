1fd40ae7a505cd129926143ed01e93d5b85e8369
blanksyy <81037708+blanksyy@users.noreply.github.com>
2021-11-25 17:48:19 +0100

    Help menu added.

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -19,19 +19,36 @@ public class Menu extends MouseAdapter  {
         int my = e.getY();
 
         //Play button
-        if(mouseOver(mx, my, 325, 200, 250, 64)){
-            game.gameState = Game.STATE.Game;
-            //Adds player to the game
-            handler.addObject(new Player(Game.WIDTH / 2 - 32, Game.HEIGHT / 2 - 32, ID.player, handler));
-            //Adds level 1 enemy to the game
-            handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH), r.nextInt(Game.HEIGHT), ID.BasicEnemy, handler));
+
+        if(game.gameState == Game.STATE.Menu) {
+
+
+            if (mouseOver(mx, my, 325, 400, 250, 64)) {
+                game.gameState = Game.STATE.Game;
+                //Adds player to the game
+                handler.addObject(new Player(Game.WIDTH / 2 - 32, Game.HEIGHT / 2 - 32, ID.player, handler));
+                //Adds level 1 enemy to the game
+                handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH), r.nextInt(Game.HEIGHT), ID.BasicEnemy, handler));
+            }
+
+            //Help button
+            if (mouseOver(mx, my, 325, 300, 250, 64)) {
+                game.gameState = Game.STATE.Help;
+            }
+            //Quit button
+            else if (mouseOver(mx, my, 325, 400, 250, 64)) {
+                System.exit(1);
+            }
         }
 
-        //Quit button
-
-        if(mouseOver(mx, my, 325, 400, 250, 64)) {
-            System.exit(1);
+        //Back button
+        if(game.gameState == Game.STATE.Help){
+            if(mouseOver(mx, my, 325, 400, 250, 64)){
+                game.gameState = Game.STATE.Menu;
+                return;
+            }
         }
+
     }
 
     public void mouseReleased(MouseEvent e){
@@ -55,23 +72,39 @@ public class Menu extends MouseAdapter  {
     }
 
     public void render(Graphics g){
-        g.setColor(Color.white);
 
-        Font fnt = new Font("Arial", 1, 50);
-        Font fnt2 = new Font("Arial", 1, 30);
+        if(game.gameState == Game.STATE.Menu) {
 
-        g.setFont(fnt);
-        g.drawString("Menu", 400, 125);
-        g.setFont(fnt2);
+            g.setColor(Color.white);
 
-        g.drawRect(325, 200, 250, 64);
-        g.drawString("Play", 420, 240);
+            Font fnt = new Font("Arial", 1, 50);
+            Font fnt2 = new Font("Arial", 1, 30);
 
-        g.drawRect(325, 300, 250, 64);
-        g.drawString("Help", 420, 340);
+            g.setFont(fnt);
+            g.drawString("Menu", 400, 125);
+            g.setFont(fnt2);
 
-        g.drawRect(325, 400, 250, 64);
-        g.drawString("Quit", 420, 440);
+            g.drawRect(325, 200, 250, 64);
+            g.drawString("Play", 420, 240);
+
+            g.drawRect(325, 300, 250, 64);
+            g.drawString("Help", 420, 340);
+
+            g.drawRect(325, 400, 250, 64);
+            g.drawString("Quit", 420, 440);
+        } else if(game.gameState == Game.STATE.Help){
+            g.setColor(Color.white);
+            Font fnt = new Font("Arial", 1, 50);
+            Font fnt2 = new Font("Arial", 1, 30);
+
+            g.setFont(fnt);
+            g.drawString("Help", 400, 125);
+
+            g.setFont(fnt2);
+            g.drawRect(325, 400, 250, 64);
+            g.drawString("Back", 420, 440);
+
+        }
 
     }
 }
